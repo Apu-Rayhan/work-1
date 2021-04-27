ace051c4011a98ac4f0b1f4194c14ced10670722
Apu-Rayhan <81389988+Apu-Rayhan@users.noreply.github.com>
2021-04-28 02:53:25 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const toggle = () => {
+    if (document.getElementById('navbar').style.width === "200px") {
+        document.getElementById('navbar').style.width = "0";
+       
+    }
+    else{
+        document.getElementById('navbar').style.width = "200px";
+       
+    }
+}
